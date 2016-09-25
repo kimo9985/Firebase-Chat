78654872aec7768e9879e82709cf06dc25bca349
Kimo <jimtrinh9985@gmail.com>
2016-09-25 00:37:38 -0700

    feat:  Sign in Activity

---
+++ app/src/main/java/com/jimtrinh9985gmail/firebasechat/CodelabPreferences.java
@@ -0,0 +1,11 @@
+package com.jimtrinh9985gmail.firebasechat;
+
+/**
+ * Created by Kimo on 9/24/2016.
+ */
+public class CodelabPreferences {
+
+    public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
---
+++ app/src/main/java/com/jimtrinh9985gmail/firebasechat/FriendlyMessage.java
@@ -0,0 +1,53 @@
+package com.jimtrinh9985gmail.firebasechat;
+
+/**
+ * Created by Kimo on 9/24/2016.
+ */
+public class FriendlyMessage {
+
+    private String id;
+    private String text;
+    private String name;
+    private String photoUrl;
+
+    public FriendlyMessage() {
+    }
+
+    public FriendlyMessage(String text, String name, String photoUrl) {
+        this.text = text;
+        this.name = name;
+        this.photoUrl = photoUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
